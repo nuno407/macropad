a3a10b03b4d8779af6447961cfd678e9d0604583
nuno407 <nuno.sousa3@bosch.com>
2022-12-21 10:48:44 +0000

    Update VSCode configs

--- user/custom_apps/vscode_app.py
+++ user/custom_apps/vscode_app.py
@@ -13,7 +13,7 @@ class VSCodeApp(BaseApp):
     # First row
     key_0 = Key("Comment", COLOR_NUMPAD, Press(Keycode.LEFT_CONTROL, Keycode.FORWARD_SLASH))
     key_1 = Key("Format", COLOR_NUMPAD, Press(Keycode.LEFT_SHIFT, Keycode.LEFT_ALT, Keycode.F))
-    key_2 = Key("Terminal", COLOR_NUMPAD, Press(Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.GRAVE_ACCENT))
+    key_2 = Key("Term", COLOR_NUMPAD, Press(Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.GRAVE_ACCENT))
 
     # Second row
     key_3 = Key("Cmd", COLOR_NUMPAD, Press(Keycode.LEFT_CONTROL, Keycode.LEFT_SHIFT, Keycode.P))
@@ -21,11 +21,11 @@ class VSCodeApp(BaseApp):
     key_5 = Key("6", COLOR_NUMPAD, Text("6"), double_tap_command=Text("-"))
 
     # Third row
-    key_6 = Key("1", COLOR_NUMPAD, Text("1"))
-    key_7 = Key("2", COLOR_NUMPAD, Text("2"))
-    key_8 = Key("3", COLOR_NUMPAD, Text("3"), double_tap_command=Text("+"))
+    key_6 = Key("InDef", COLOR_NUMPAD, Press(Keycode.LEFT_ALT, Keycode.F12))
+    key_7 = Key("SideDef", COLOR_NUMPAD,  Press(Keycode.LEFT_CONTROL, Keycode.LEFT_ALT))
+    key_8 = Key("BackDef", COLOR_NUMPAD, Press(Keycode.LEFT_ALT, Keycode.LEFT_ARROW))
 
     # Fourth row
-    key_9 = Key(".", COLOR_8, Text("."))
-    key_10 = Key("0", COLOR_NUMPAD, Text("0"))
-    key_11 = Key("Enter", COLOR_WARNING, Press(Keycode.KEYPAD_ENTER))
+    key_9 = Key("CTRL", COLOR_NUMPAD, Press(Keycode.LEFT_CONTROL))
+    key_10 = Key()
+    key_11 = Key("ESC", COLOR_WARNING, Press(Keycode.ESCAPE))
